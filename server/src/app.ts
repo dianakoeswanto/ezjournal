b674fee847f4ae1a67c73520fecb08c7f82f4df3
Diana Koeswanto <d.koeswanto@gmail.com>
2021-07-25 09:52:55 +1000

    Fixed relative pathing to client build.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,22 @@
-import express, { Express } from "express"
-import mongoose from "mongoose"
-import cors from "cors"
-import routes from "./routes"
-import path from "path"
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import routes from "./routes";
+import path from "path";
  
-const app: Express = express()
- 
-const PORT: string | number = process.env.PORT || 3001
-app.use(cors())
+const app: Express = express();
+const PORT: string | number = process.env.PORT || 3001;
+
+app.use(cors());
 app.use(routes);
-app.use(express.static(path.join(__dirname, '../js')))
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../../../client/build')));
+
+// All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../js'))
-})
+  res.sendFile(path.resolve(__dirname, '../../../client/build', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ezjournal", {
   useNewUrlParser: true,
@@ -21,4 +25,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ezjournal", {
     app.listen(PORT, () =>
       console.log(`Server running on http://localhost:${PORT}`)
     );
-}).catch(error => {throw error;})
+}).catch(error => {throw error;});
