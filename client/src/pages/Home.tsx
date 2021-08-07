edd3dcb5197902dac2576f7a809694df6a54f797
Diana Koeswanto <d.koeswanto@gmail.com>
2021-08-07 14:28:17 +1000

    Home to either show my children/classes

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,25 +1,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import ListView, { ListViewData } from '../component/ListView';
-import { IChild, IHomeData, IUser } from '../types/types';
+import { IChild, IClass, IHomeData, IUser } from '../types/types';
 import AddChild from './AddChild';
 import { useChildren } from '../store/store';
 import { useCurrentUser } from '../hooks/use-current-user';
 import { useAuth0 } from '@auth0/auth0-react';
 
-
-const getChildren = async (user: IUser, token: string): Promise<IChild[]> => {
-    return (await axios.get(`/api/children/parent=${user.id}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    })).data.children as IChild[];
-}
-
 const transformChildren = (children: IChild[]): ListViewData[] => children.map((child) => ({
     id: child.id,
     displayName: `${child.firstname} ${child.lastname}`,
     linkURL: `/children/${child.id}/classes`
+}));
+
+const transformClasses = (classes: IClass[]): ListViewData[] => classes.map((klass) => ({
+    id: klass._id,
+    displayName: `${klass.student.firstname} ${klass.student.lastname} - ${klass.classDay} ${klass.classTime}`,
+    linkURL: `/classes/${klass._id}/lessons`
 }));
 
 const getHomeData = async (userId: string, token: string): Promise<IHomeData> => {
@@ -34,25 +31,35 @@ const Home = (): React.ReactElement => {
     const user = useCurrentUser();
     const { getAccessTokenSilently } = useAuth0();
     const [{ children }, { set }] = useChildren();
+    const [classes, setClasses] = useState<IClass[]>([]);
+    let returnedUser: IUser = {id: '', email: '', name: ''};
 
     useEffect(() => {
         getAccessTokenSilently()
             .then((token) => {
-                getHomeData(user.id, token).then(data => {
-                    if(data.user.isParent) {
-                        set(data.children as IChild[]);
-                    }
-                })
-                // getChildren(user, token).then(children => set(children));
+                getHomeData(user.id, token)
+                    .then(data => {
+                        returnedUser = data.user;
+                        console.log(returnedUser.isParent);
+                        if(data.user.isParent) {
+                            set(data.children as IChild[]);
+                        } else {
+                            setClasses(data.classes as IClass[])
+                        }
+                    })
             });
     }, []);
    
     return (
-        <ListView 
-            title="My Children" 
-            displayData={transformChildren(children)}
-            addButton={<AddChild />}
-        />
+        <div>
+            <ListView 
+                title="My Children" 
+                displayData={transformChildren(children)}
+                addButton={<AddChild />}
+            />
+        </div>
+
+        
     );
 }
 
