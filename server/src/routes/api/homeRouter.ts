e9fc019f7759b0f45e67e8045f09079fdb2cb952
Diana Koeswanto <d.koeswanto@gmail.com>
2021-08-07 21:24:21 +1000

    Handle partially created user.

--- server/src/routes/api/homeRouter.ts
+++ server/src/routes/api/homeRouter.ts
@@ -7,15 +7,24 @@ import { getCurrentUser } from '../util';
 const homeRouter: Router = Router();
 
 homeRouter.get('/', async(request, response) => {
-    const auth0User = await getCurrentUser(request);
-    let user: IUser | null = await User.findOne({ email: auth0User.email });
-    if(!user) {
+    const auth0User = await getCurrentUser(request); // Get user info from auth0.
+
+    let user: IUser | null = await User.findOne({ email: auth0User.email }); // Map auth0 user to *our* user
+    if (user) {
+        if(!user.name) {
+            // Partially created user, likely created when a new class is created and we couldn't find an existing user with
+            // matching email.
+            user.name = `${auth0User.firstname} ${auth0User.lastname}`;
+            await user.save();
+        }
+    } else {
         user = await User.create({
             email: auth0User.email,
             name: `${auth0User.firstname} ${auth0User.lastname}`,
         });
     }
 
+    // Now we have a user, find children and classes related to the user.
     const children: IStudent[] = await Student.find({parent: user!.id});
     const transformedChildren = children.map((child) => {
         return {
