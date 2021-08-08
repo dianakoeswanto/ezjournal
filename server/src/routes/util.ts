e3dd3b2215589c481f923494210752f14cca5e5f
Diana Koeswanto <d.koeswanto@gmail.com>
2021-08-08 23:34:32 +1000

    Cache user info because we were getting rate limited.

--- server/src/routes/util.ts
+++ server/src/routes/util.ts
@@ -7,13 +7,28 @@ export interface Auth0User {
     email: string,
 };
 
+const userInfoCache = new Map();
 export const getCurrentUser = async (request: Request): Promise<Auth0User> => {
+    // @ts-ignore
+    const cachedUser = userInfoCache.get(request.user.sub);
+    if (cachedUser) {
+        console.log('User info cache hit!');
+        return {
+            email: cachedUser.email,
+            firstname: cachedUser.given_name,
+            lastname: cachedUser.family_name,
+        };
+    }
+
+    console.log('User info cache missed!');
     const {data: user} = await axios.get('https://ezjournal.au.auth0.com/userinfo', {
         headers: {
             Authorization: `${request.header('Authorization')}`
         },
     });
 
+    // @ts-ignore
+    userInfoCache.set(request.user.sub, user);
     return {
         email: user.email,
         firstname: user.given_name,
